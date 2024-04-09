43f1271ced922d579f06b26fbbc70fef6027250f
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-04-09 18:32:22 +0800

    fix| consumer模块路由文件位置纠正| ZedFeorius

--- surf/modules/consumer/routing.py
+++ surf/modules/consumer/routing.py
@@ -3,9 +3,8 @@
 from django.urls import re_path
 from surf.modules.consumer import (ChatConsumer, KeyExchangeConsumer, LoginConsumer)
 
-websocket_urlpatterns = [
+ulr_patterns = [
     re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
     re_path(r'ws/login/$', LoginConsumer.as_asgi()),
     re_path(r'ws/key_exchange/$', KeyExchangeConsumer.as_asgi()),
-
 ]
