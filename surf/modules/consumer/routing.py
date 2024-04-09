57f412397ea8154aff5f98eec0d823e786917fa6
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-09 22:13:51 +0800

    fix |ulr -> url 错别字修正 |ZedFeorius

--- surf/modules/consumer/routing.py
+++ surf/modules/consumer/routing.py
@@ -3,7 +3,7 @@
 from django.urls import re_path
 from surf.modules.consumer import (ChatConsumer, KeyExchangeConsumer, LoginConsumer)
 
-ulr_patterns = [
+url_patterns = [
     re_path(r'ws/chat/$', ChatConsumer.as_asgi()),
     re_path(r'ws/login/$', LoginConsumer.as_asgi()),
     re_path(r'ws/key_exchange/$', KeyExchangeConsumer.as_asgi()),
