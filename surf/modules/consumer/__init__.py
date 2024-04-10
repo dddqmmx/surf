61ea8b93a5e1cd324d1a78074bee356d0f1a97d3
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-10 21:36:13 +0800

    REFACTOR |Consumer重构|ZedFeorius

--- surf/modules/consumer/__init__.py
+++ surf/modules/consumer/__init__.py
@@ -1,4 +1,2 @@
 # notifications/consumers/__init__.py
-from .chat_consumer import ChatConsumer
-from .key_exchange_consumer import KeyExchangeConsumer
-from .login_consumer import LoginConsumer
+from models import(ChatConsumer, KeyExchangeConsumer, LoginConsumer)
