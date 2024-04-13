ef265aade4cd5764b89582755d6859edf46205e2
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-13 22:26:08 +0800

    ADD |将UserService注册到services中 |ZedFeorius

---
+++ surf/modules/consumer/services/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created By      : ZedFeorius
+Created Time    : 2024/4/13 12:29
+File Name       : __init__
+Project Name    : surf-websocket
+Last Edit Time  : 
+"""
+from .user_service import UserService
