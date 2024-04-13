8093876d5ce56f99f90063b5fee5638c9e6c98d2
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-13 22:26:57 +0800

    UPDATE |注册在util中的BaseDBPG更名 Pg -> BaseModel |ZedFeorius

--- surf/modules/util/__init__.py
+++ surf/modules/util/__init__.py
@@ -5,6 +5,6 @@ Created Time    : 2024/3/26 16:39
 File Name       : __init__.py
 Last Edit Time  : 
 """
-from .base_db_pg import BaseDBPG as Pg
+from .base_db_pg import BaseDBPG as BaseModel
 from .session_util import Session
 from .es_client import ESClient as Ec
