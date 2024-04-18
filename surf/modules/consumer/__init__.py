468a8bfb3bc7378175abd8a68b9eccc08edff350
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-04-18 19:12:42 +0800

    FEAT| 新增server相关操作（server数据库查询模型，server_service，server_consumer）| ZedFeorius

--- surf/modules/consumer/__init__.py
+++ surf/modules/consumer/__init__.py
@@ -2,3 +2,4 @@
 from .user_consumer import UserConsumer
 from .chat_consumer import ChatConsumer
 from .key_exchange_consumer import KeyExchangeConsumer
+from .base_consumer import BaseConsumer
--- surf/modules/consumer/models/__init__.py
+++ surf/modules/consumer/models/__init__.py
@@ -1,1 +1,2 @@
 from .user_model import UserModel
+from .server_model import ServerModel
