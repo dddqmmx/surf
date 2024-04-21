cf6df76d488e38262f1d273714dbc1a6a3f50518
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-22 00:26:42 +0800

    FIX | 新增从text_data中获取不到server的异常处理|ZedFeorius

--- surf/modules/consumer/services/server_service.py
+++ surf/modules/consumer/services/server_service.py
@@ -34,57 +34,63 @@ class ServerService(object):
         server_id = None
         try:
             if text_data:
-                filter = text_data["server"]
-                if isinstance(filter, dict):
-                    filter["owner_id"] = Session.get_session_by_id(filter["session_id"]).get("user_id")
-                    server_filter = {f"c_{k}": v for k, v in filter.items()}
-                    server_id = self.__serverModel.save_server(server_filter)
-                    if server_id:
-                        permissions = self.__roleModel.get_all_permissions()
-                        filters = {"c_server_id": server_id, "c_name": "服务器拥有者", "c_permissions": json.dumps([item['id'] for item in permissions])}
-                        role_id = self.__roleModel.create_role(filters)
-                        if role_id:
-                            filters = {"c_server_id": server_id, "c_user_id": server_filter["c_owner_id"], "c_roles": json.dumps([role_id])}
-                            res = self.__serverModel.save_server_user(filters=filters)
-                            if res:
-                                filters = [{"c_server_id": server_id, "c_group_name": "文字频道分组"},
-                                           {"c_server_id": server_id, "c_group_name": "语音频道分组"}]
-                                res = self.__channelModel.save_channel_group(filters)
-                                if len(res) == 2:
-                                    filters = [
-                                        {
-                                            "c_group_id": res[0],
-                                            "c_name": "默认文字频道",
-                                            "c_type": "text",
-                                            "c_description": "这是一个文字频道",
-                                            "c_create_by": server_filter["c_owner_id"]
-                                        },
-                                        {
-                                            "c_group_id": res[1],
-                                            "c_name": "默认语音频道",
-                                            "c_type": "voice",
-                                            "c_description": "这是一个语音频道",
-                                            "c_create_by": server_filter["c_owner_id"]
-                                        }
-                                    ]
-                                    res = self.__channelModel.save_channel(filters)
-                                    if res is not False and len(res) == 2:
-                                        respond_json["message"] = True
-                                    else:
-                                        error_flag = True
-                                        print("create channel failed")
-                                print("成功")
+                filters = text_data.get("server", None)
+                if filters:
+                    if isinstance(filters, dict):
+                        filters["owner_id"] = Session.get_session_by_id(filter["session_id"]).get("user_id")
+                        server_filter = {f"c_{k}": v for k, v in filter.items()}
+                        server_id = self.__serverModel.save_server(server_filter)
+                        if server_id:
+                            permissions = self.__roleModel.get_all_permissions()
+                            filters = {"c_server_id": server_id, "c_name": "服务器拥有者",
+                                       "c_permissions": json.dumps([item['id'] for item in permissions])}
+                            role_id = self.__roleModel.create_role(filters)
+                            if role_id:
+                                filters = {"c_server_id": server_id, "c_user_id": server_filter["c_owner_id"],
+                                           "c_roles": json.dumps([role_id])}
+                                res = self.__serverModel.save_server_user(filters=filters)
+                                if res:
+                                    filters = [{"c_server_id": server_id, "c_group_name": "文字频道分组"},
+                                               {"c_server_id": server_id, "c_group_name": "语音频道分组"}]
+                                    res = self.__channelModel.save_channel_group(filters)
+                                    if len(res) == 2:
+                                        filters = [
+                                            {
+                                                "c_group_id": res[0],
+                                                "c_name": "默认文字频道",
+                                                "c_type": "text",
+                                                "c_description": "这是一个文字频道",
+                                                "c_create_by": server_filter["c_owner_id"]
+                                            },
+                                            {
+                                                "c_group_id": res[1],
+                                                "c_name": "默认语音频道",
+                                                "c_type": "voice",
+                                                "c_description": "这是一个语音频道",
+                                                "c_create_by": server_filter["c_owner_id"]
+                                            }
+                                        ]
+                                        res = self.__channelModel.save_channel(filters)
+                                        if res is not False and len(res) == 2:
+                                            respond_json["message"] = True
+                                        else:
+                                            error_flag = True
+                                            print("create channel failed")
+                                    print("成功")
+                                else:
+                                    print("add creator to server failed")
+                                    error_flag = True
                             else:
-                                print("add creator to server failed")
+                                print("add server role failed")
                                 error_flag = True
                         else:
-                            print("add server role failed")
+                            print("server create failed")
                             error_flag = True
                     else:
-                        print("server create failed")
+                        print("type error")
                         error_flag = True
                 else:
-                    print("type error")
+                    print("未获取到server参数")
                     error_flag = True
             else:
                 print("text_data参数错误")
