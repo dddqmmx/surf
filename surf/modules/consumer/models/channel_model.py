11754603393332417b63b08a5ef7dc4edb9a2ea9
ZedFeorius <zedfeorius7879@gmail.com>
2024-04-23 00:34:21 +0800

    FEAT | 根据服务器id获取对应的频道分组|ZedFeorius

--- surf/modules/consumer/models/channel_model.py
+++ surf/modules/consumer/models/channel_model.py
@@ -6,6 +6,8 @@ File Name       : channel_model
 Project Name    : surf-websocket
 Last Edit Time  : 
 """
+import traceback
+
 from surf.modules.util import BaseModel
 
 
@@ -20,3 +22,13 @@ class ChannelModel(BaseModel):
     def save_channel_group(self, filters) -> str or bool:
         return self._pg.save("t_channel_groups", filters, primary="c_group_id", return_id=True,
                              return_id_clumn="c_group_id")
+
+    def get_channel_group_by_server_id(self, server_id):
+        res = []
+        try:
+            sql = """SELECT c_group_id as id, c_group_name as name FROM t_channel_groups WHERE c_server_id = %s"""
+            res.extend(self._pg.query(sql, [server_id]))
+        except Exception as e:
+            print(f"""{e}\n{traceback.format_exc()}""")
+        finally:
+            return res
