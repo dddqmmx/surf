bccae4a48aef3bd80e1d935309b99474058a941f
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-04-22 17:22:55 +0800

    ADD| 新增es chat索引公众模板| ZedFeorius

--- surf/appsGlobal.py
+++ surf/appsGlobal.py
@@ -5,3 +5,16 @@ Created Time    : 2024/3/26 17:27
 File Name       : appsGloble.py
 Last Edit Time  : 
 """
+
+
+CHAT_TEMP = {
+    "_id": "",
+    "_index": "chat_message",
+    "_source": {
+        "chat_id": "",
+        "type": "",
+        "content": "",
+        "user_id": "",
+        "chat_time": ""
+    }
+}
