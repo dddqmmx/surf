f665b2b4a5c2c89e87a956c794e35da341389041
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-04-22 11:18:52 +0800

    UPDATE| SQL新增初始测试数据| ZedFeorius

--- sql/new_surf_sql.sql
+++ sql/new_surf_sql.sql
@@ -191,8 +191,21 @@ BEFORE INSERT OR UPDATE ON t_roles
 FOR EACH ROW EXECUTE FUNCTION check_permissions_exist();
 
 
-INSERT INTO t_users(c_user_id, c_nickname, c_public_key, c_user_info) VALUES ('e2cfa16b-c7a3-46f0-9995-22e2ae333e3e','80e6701cacfccc7e5c0b1767a466b993','LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUF1YXNhRHcxVnpmeTJKU3BnVTMrRApEeTB0QzVadXFNSjhOanpnQk85UEtBcVU4QXdiem1WdEY5M0tieU8xd0xSTGJySXkxR0tJVlBkd0ZwOVVuNlZ2ClJNeHIxQ0hmRHY2WFFpYUpmbjJLZ1FaQ1lDcmo4REorcG84N2FneVF3R2s2ZEpDc2ZNOHoxenNqZ3dQQkxyTHMKcHo4aWZGcGNJUkkvWDF1a3RUS1JzaHhWVkh0OGtYYjVrRDl6SUM1ekRRcVBMeVc1TnFoUFZEbU1UdnFvdG05SwpicllqSnltbi9xaTJuVGplcDNaKzFxbHp5NmdPRXF4S3pralZwd1lyMlNsbWFEQU5iT2RKanJuQVdsNHFVNXN3CnorMzRWTklkaHJTSnhXRjBRR2MzaU82UENZS29mYWVsUWJYZjI3UTF5Uk1TdTYxL0hqdmhWUWNvTnYxeTFQT2cKSllkb2xiREwwYW9pWFNXOWZOK2hHd3RmZjczdjIwemV1N3RGWWowbTViczFFd3JvK2RjUjZsQmFUUHBRZnZDLwpydWVKaHpNZzAvZ3hvTFFBakI3eTlNVnVQYTNEUFVpSVFpdktHeGtUN3ZKdlN5WGdCb29KOHZiNE16dElIb1BGCngxb3RmQ05PajhVdGlwdG4rOFFTUXZ0UXJFdklZdVpaOERnSUd1NmtRVzFEMzZhRG1iVFdra00vTUw3UmRGN20KMVpOb3JBMzFnT3JQWnByTlRQUzlxRWtTY2x4dWxoZ3VHd2ZYUEcwTlk0NVZxNlhoUW1MR0NGMFVaWHhrRXllagpXdStKVjE0ZTJRNUlzTTdtaFpOQlp5SjRxSjZ1MFQxZ0RUMnZWWDN1a1o2dmtnNlZrWDdxTFFNRDk1Z0cwZjdpCnBKMXRQWTVzMG1oTmRUSUpvODBzTk5zQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=','{"email": "woshishabi@gmail.com", "phone": "1145141919810"}')
+INSERT INTO public.t_users(c_user_id, c_nickname, c_public_key, c_user_info) VALUES ('e2cfa16b-c7a3-46f0-9995-22e2ae333e3e','80e6701cacfccc7e5c0b1767a466b993','LS0tLS1CRUdJTiBQVUJMSUMgS0VZLS0tLS0KTUlJQ0lqQU5CZ2txaGtpRzl3MEJBUUVGQUFPQ0FnOEFNSUlDQ2dLQ0FnRUF1YXNhRHcxVnpmeTJKU3BnVTMrRApEeTB0QzVadXFNSjhOanpnQk85UEtBcVU4QXdiem1WdEY5M0tieU8xd0xSTGJySXkxR0tJVlBkd0ZwOVVuNlZ2ClJNeHIxQ0hmRHY2WFFpYUpmbjJLZ1FaQ1lDcmo4REorcG84N2FneVF3R2s2ZEpDc2ZNOHoxenNqZ3dQQkxyTHMKcHo4aWZGcGNJUkkvWDF1a3RUS1JzaHhWVkh0OGtYYjVrRDl6SUM1ekRRcVBMeVc1TnFoUFZEbU1UdnFvdG05SwpicllqSnltbi9xaTJuVGplcDNaKzFxbHp5NmdPRXF4S3pralZwd1lyMlNsbWFEQU5iT2RKanJuQVdsNHFVNXN3CnorMzRWTklkaHJTSnhXRjBRR2MzaU82UENZS29mYWVsUWJYZjI3UTF5Uk1TdTYxL0hqdmhWUWNvTnYxeTFQT2cKSllkb2xiREwwYW9pWFNXOWZOK2hHd3RmZjczdjIwemV1N3RGWWowbTViczFFd3JvK2RjUjZsQmFUUHBRZnZDLwpydWVKaHpNZzAvZ3hvTFFBakI3eTlNVnVQYTNEUFVpSVFpdktHeGtUN3ZKdlN5WGdCb29KOHZiNE16dElIb1BGCngxb3RmQ05PajhVdGlwdG4rOFFTUXZ0UXJFdklZdVpaOERnSUd1NmtRVzFEMzZhRG1iVFdra00vTUw3UmRGN20KMVpOb3JBMzFnT3JQWnByTlRQUzlxRWtTY2x4dWxoZ3VHd2ZYUEcwTlk0NVZxNlhoUW1MR0NGMFVaWHhrRXllagpXdStKVjE0ZTJRNUlzTTdtaFpOQlp5SjRxSjZ1MFQxZ0RUMnZWWDN1a1o2dmtnNlZrWDdxTFFNRDk1Z0cwZjdpCnBKMXRQWTVzMG1oTmRUSUpvODBzTk5zQ0F3RUFBUT09Ci0tLS0tRU5EIFBVQkxJQyBLRVktLS0tLQo=','{"email": "woshishabi@gmail.com", "phone": "1145141919810"}')
 
-INSERT INTO t_permissions (c_permission_id, c_description) VALUES (1, 'Permission 1');
-INSERT INTO t_permissions (c_permission_id, c_description) VALUES (2, 'Permission 2');
-INSERT INTO t_permissions (c_permission_id, c_description) VALUES (3, 'Permission 3');
+INSERT INTO public.t_permissions (c_permission_id, c_description) VALUES (1, 'Permission 1');
+INSERT INTO public.t_permissions (c_permission_id, c_description) VALUES (2, 'Permission 2');
+INSERT INTO public.t_permissions (c_permission_id, c_description) VALUES (3, 'Permission 3');
+
+INSERT INTO public.t_servers (c_server_id, c_description, c_name, c_owner_id, c_create_time, c_icon_url, c_is_active, c_is_private) VALUES ('fdad3e5e-818d-4201-8b07-4734da71d9b7','傻逼','hololive','e2cfa16b-c7a3-46f0-9995-22e2ae333e3e','1713630327',NULL,true,true);
+
+INSERT INTO public.t_roles (c_role_id, c_server_id, c_name, c_permissions) VALUES ('bdabb221-ebe9-478d-bb42-86578cb2e4c0', 'fdad3e5e-818d-4201-8b07-4734da71d9b7', '服务器拥有者', '[1, 2, 3]');
+
+INSERT INTO public.t_server_members (c_id, c_server_id, c_user_id, c_roles) VALUES ('b8dc425a-f238-4bf6-85cc-e17c27833a63', 'fdad3e5e-818d-4201-8b07-4734da71d9b7', 'e2cfa16b-c7a3-46f0-9995-22e2ae333e3e', '["bdabb221-ebe9-478d-bb42-86578cb2e4c0"]');
+
+INSERT INTO public.t_channel_groups (c_group_id, c_server_id, c_group_name) VALUES ('97096b43-2de4-43ac-9024-ebb63ed3ec1e', 'fdad3e5e-818d-4201-8b07-4734da71d9b7', '文字频道分组');
+INSERT INTO public.t_channel_groups (c_group_id, c_server_id, c_group_name) VALUES ('680c0836-58c4-43fb-8fb8-23a2103ffcdd', 'fdad3e5e-818d-4201-8b07-4734da71d9b7', '语音频道分组');
+
+
+INSERT INTO public.t_channels (c_channel_id, c_group_id, c_name, c_type, c_description, c_create_by, c_create_time) VALUES ('aa6cd21b-7080-4e65-9059-8a6a8c303cbb', '97096b43-2de4-43ac-9024-ebb63ed3ec1e', '默认文字频道', 'text', '这是一个文字频道', 'e2cfa16b-c7a3-46f0-9995-22e2ae333e3e', 1713630328);
+INSERT INTO public.t_channels (c_channel_id, c_group_id, c_name, c_type, c_description, c_create_by, c_create_time) VALUES ('0362e80c-839b-4ee6-9e77-c2cb6668c961', '680c0836-58c4-43fb-8fb8-23a2103ffcdd', '默认语音频道', 'voice', '这是一个语音频道', 'e2cfa16b-c7a3-46f0-9995-22e2ae333e3e', 1713630328);
