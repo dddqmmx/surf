61692912b667a2185d5f7d039343d65803a29612
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-05-08 18:23:21 +0800

    DELETE| 清除废旧代码| ZedFeorius

--- surf/modules/consumer/__init__.py
+++ surf/modules/consumer/__init__.py
@@ -1,6 +1,1 @@
-# notifications/consumers/__init__.py
-from .user_consumer import UserConsumer
-from .chat_consumer import ChatConsumer
-from .server_consumer import ServerConsumer
-from .key_exchange_consumer import KeyExchangeConsumer
 from .surf_consumer import SurfConsumer
