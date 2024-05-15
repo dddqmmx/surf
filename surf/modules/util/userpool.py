e6d5485adf866422d47ca2766baea9a55893ebf3
ZedFeorius <zedfeorius7879@gmail.com>
2024-05-15 10:42:21 +0800

    FEAT | 广播功能完善|ZedFeorius

--- surf/modules/util/userpool.py
+++ surf/modules/util/userpool.py
@@ -8,10 +8,11 @@ Last Edit Time  :
 import asyncio
 import datetime
 import json
-from typing import Union, Tuple, Dict
+from typing import Union, Tuple, Dict, Any, Coroutine, List
 from .session_util import Session
 from .surf_user import SurfUser
 from surf.appsGlobal import logger, get_logger, setResult, errorResult
+from surf.modules.consumer.services import UserService, ServerService
 
 con_log = get_logger('connections')
 
@@ -27,9 +28,11 @@ class UserPool(object):
     def __init__(self):
         if not hasattr(self, '_initialized'):
             self.__connected_user: Dict[str, SurfUser] = {}
-            self.__broadcast_map: Dict[str, Dict[str, SurfUser]] = {}
+            self.__broadcast_map: Dict[str, Dict[str, List[SurfUser]]] = {}
             self.lock = asyncio.Lock()
             self._initialized = True
+            self.__user_service = UserService()
+            self.__server_service = ServerService()
             con_log.info(f'UserPool initialized:{datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")}')
 
     def get_users(self):
@@ -38,37 +41,54 @@ class UserPool(object):
     def get_broadcast_by_channel_id(self, channel_id):
         return self.__broadcast_map.copy()[channel_id]
 
-    def connect_user_to_pool(self, session, user) -> bool:
-        session_id = session.session_id
-        if self.__connected_user.get(session_id, None):
-            con_log.info(f"session_id:{session_id} has already logged in, user\'s info might be infiltrated")
-            return False
-        else:
-            for k, user in self.get_users().items():
-                if user.check_user_id_by_session_id(session_id):
-                    con_log.info(
-                        f"session_id:{session_id}'s bound user_id has logged in already"
-                        f", user\'s info might be infiltrated")
-                    return False
-            self.__connected_user[session_id] = user
-            return True
+    async def connect_user_to_pool(self, session, user):
+        async with self.lock:
+            session_id = session.session_id
+            if self.__connected_user.get(session_id, None):
+                con_log.info(f"session_id:{session_id} has already logged in, user\'s info might be infiltrated")
+                return False
+            else:
+                for k, user in self.get_users().items():
+                    if user.check_user_id_by_session_id(session_id):
+                        con_log.info(
+                            f"session_id:{session_id}'s bound user_id has logged in already"
+                            f", user\'s info might be infiltrated")
+                        return False
+                self.__connected_user[session_id] = user
+                return True
 
-    def access_new_user(self, session, consumer, return_id=False) -> Union[bool, Tuple[bool, str]]:
+    async def access_new_user(self, session, consumer, return_id=False):
         user_id = session.get('user_id')
         surf_user = SurfUser(user_id, consumer)
-        flag = self.connect_user_to_pool(session, surf_user)
+        flag = await self.connect_user_to_pool(session, surf_user)
+        await self.connect_user_to_broadcast_map(session, surf_user)
         if return_id:
             return flag, session.session_id
         return flag
 
     def broadcast_to_all_user_in_channel(self, text_data):
         channel_id = text_data['channel_id']
-        for k, user in self.get_broadcast_by_channel_id(channel_id).items():
-            user.broadcast(text_data)
+        if channel_id in self.__broadcast_map:
+            for k, user_list in self.get_broadcast_by_channel_id(channel_id).items():
+                asyncio.create_task(user.broadcast(text_data) for user in user_list)
 
-    def detach_user_from_pool_by_session_id(self, session_id):
-        del self.__connected_user[session_id]
-        con_log.info(f'session_id:{session_id} has disconnect from surf')
+    async def connect_user_to_broadcast_map(self, session: Session, surf_user):
+        user_id = session.get('user_id')
+        ids = self.__server_service.get_channels_by_user_id(user_id)
+        if ids is not False:
+            async with self.lock:
+                for channel_id in ids:
+                    server_id = self.__server_service.get_server_by_channel_id(channel_id['id'])[0]['id']
+                    if not self.__broadcast_map.get(server_id, None):
+                        self.__broadcast_map[server_id] = {}
+                    if not self.__broadcast_map[server_id].get(channel_id['id'], None):
+                        self.__broadcast_map[server_id][channel_id['id']] = []
+                    self.__broadcast_map[server_id][channel_id['id']].append(surf_user)
+
+    async def detach_user_from_pool_by_session_id(self, session_id):
+        async with self.lock:
+            del self.__connected_user[session_id]
+            con_log.info(f'session_id:{session_id} has disconnect from surf')
 
 
 def session_check(func):
@@ -79,7 +99,7 @@ def session_check(func):
         if text_data['command'] != 'login' and text_data != 'key_exchange':
             session_id = text_data.get('session_id', None)
             if session_id:
-                for k, user in UserPool().get_users():
+                for k, user in UserPool().get_users().items():
                     if user.check_user_id_by_session_id(session_id) and session_id == args[0].session_id:
                         flag = True
                         break
@@ -91,4 +111,5 @@ def session_check(func):
             logger.error(text_data)
             logger.error(f"发现无效session进行操作：{session_id}， 已拦截")
             await args[0].send(errorResult(text_data['command'], '无效session', text_data['path']))
+
     return wrapper
