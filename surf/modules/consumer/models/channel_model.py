93627a6f3881e17e496f07fa4fdc6ace0768531d
ZedFeorius <zedfeorius7879@gmail.com>
2024-05-15 10:40:53 +0800

    UPDATE | 新增返回类型规范|ZedFeorius

--- surf/modules/consumer/models/channel_model.py
+++ surf/modules/consumer/models/channel_model.py
@@ -7,6 +7,7 @@ Project Name    : surf-websocket
 Last Edit Time  : 
 """
 import traceback
+from typing import Union, List, Dict
 
 from surf.modules.util import BaseModel
 
@@ -44,6 +45,46 @@ class ChannelModel(BaseModel):
                 c_description as description
             FROM t_channels WHERE c_group_id = %s"""
             res.extend(self._pg.query(sql, [group_id]))
+        except Exception as e:
+            print(f"""{e}\n{traceback.format_exc()}""")
+        finally:
+            return res
+
+    def save_channel_members(self, filters) -> bool:
+        return self._pg.save("t_channel_members", filters)
+
+    def get_channel_ids_by_user_id(self, user_id) -> List[Dict[str, str]]:
+        res = []
+        try:
+            sql = """
+                    SELECT 
+                        c_channel_id as id
+                    FROM t_channel_members WHERE c_user_id = %s"""
+            res.extend(self._pg.query(sql, [user_id]))
+        except Exception as e:
+            print(f"""{e}\n{traceback.format_exc()}""")
+        finally:
+            return res
+
+    def get_server_by_channel_id(self, channel_id) -> List[Dict[str, str]]:
+        res = []
+        try:
+            sql = """
+            SELECT 
+                s.c_server_id as id 
+            FROM
+                t_servers as s 
+            INNER JOIN 
+                t_channel_groups as cg 
+                ON 
+                (s.c_server_id = cg.c_server_id )   
+            INNER JOIN
+                t_channels as c
+                ON 
+                (cg.c_group_id = c.c_group_id)
+            WHERE c.c_channel_id = %s
+            """
+            res.extend(self._pg.query(sql, [channel_id]))
         except Exception as e:
             print(f"""{e}\n{traceback.format_exc()}""")
         finally:
