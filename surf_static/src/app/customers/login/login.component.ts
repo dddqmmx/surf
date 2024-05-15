d7953a62711eba787a30f7bdf7a2b3d28c7a8279
dddqmmx <1538080366@qq.com>
2024-05-15 16:39:17 +0800

    UPDATE | 修改main页面指向| dddqmmx

--- surf_static/src/app/customers/login/login.component.ts
+++ surf_static/src/app/customers/login/login.component.ts
@@ -8,6 +8,7 @@ import { Router ,NavigationExtras} from '@angular/router';
 import {LocalDataService} from "../../services/local_data/local-data.service";
 import {SocketManagerService} from "../../services/socket/socket-manager.service";
 import {main} from "@angular/compiler-cli/src/main";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-login',
@@ -20,9 +21,15 @@ import {main} from "@angular/compiler-cli/src/main";
 })
 export class LoginComponent {
 
-    constructor(private cryptoService: CryptoService,private localDataService:LocalDataService,private socketMassageService:SocketManagerService,private router: Router) {
+    constructor(private cryptoService: CryptoService,private localDataService:LocalDataService,private socketManageService:SocketManagerService,private router: Router) {
         this.cryptoService = cryptoService;
         this.localDataService = localDataService;
+        this.socketManageService = socketManageService;
+    }
+
+    private subscriptions: Subscription[] = [];
+    ngOnDestroy(){
+        this.subscriptions.forEach(subscription => subscription.unsubscribe());
     }
 
     socket: WebSocket | undefined;
@@ -62,7 +69,7 @@ export class LoginComponent {
         const self = this;
         if (this.isJSON(this.fileContent)) {
             const userFile = JSON.parse(this.fileContent)
-            const toURLSubject = this.socketMassageService.getMessageSubject("user", "to_url").subscribe(
+            const toURLSubject = this.socketManageService.getMessageSubject("user", "to_url").subscribe(
                 message => {
                     const data = JSON.parse(message.data).messages;
                     console.log(data)
@@ -71,12 +78,13 @@ export class LoginComponent {
                     const address = data.address;
                     if (address == 'main'){
                         console.log('main')
-                        self.router.navigate(['main/chat']);
+                        self.router.navigate(['main']);
                         self.socket?.close();
                     }
                     toURLSubject.unsubscribe()
                 })
-            this.socketMassageService.send("user", "login", {
+            this.subscriptions.push(toURLSubject);
+            this.socketManageService.send("user", "login", {
                 "public_key": userFile.public_key,
             })
         }
@@ -87,13 +95,13 @@ export class LoginComponent {
         if (this.isJSON(this.fileContent)) {
             const userFile = JSON.parse(this.fileContent)
             const serverAddress = userFile.server_address;
-            this.socketMassageService.initializeMainConnection(serverAddress)
+            this.socketManageService.initializeMainConnection(serverAddress)
                 .then(async () => {
                     try {
                         // const keyPair = await this.cryptoService.generateKeyPair();
                         // const publicKeyString = await this.cryptoService.exportPublicKey(keyPair.publicKey);
                         // console.log(publicKeyString);
-                        // const keyExchangeSubject = this.socketMassageService.getMessageSubject("key", "key_exchange").subscribe(
+                        // const keyExchangeSubject = this.socketManageService.getMessageSubject("key", "key_exchange").subscribe(
                         //     message => {
                         //         const data = JSON.parse(message.data).messages;
                         //         const public_key = data.public_key;
@@ -101,19 +109,19 @@ export class LoginComponent {
                         //         keyExchangeSubject.unsubscribe()
                         //     }
                         // )
-                        // this.socketMassageService.send("key", "key_exchange", {
+                        // this.socketManageService.send("key", "key_exchange", {
                         //     "public_key": publicKeyString
                         // })
                         self.login()
                     } catch (err) {
-                        this.socketMassageService.disconnect()
+                        this.socketManageService.disconnect()
                         console.error(err);
                     }
                 })
                 .catch((error) => {
                     // 发生错误时的处理
                     console.error('WebSocket连接出错:', error);
-                    this.socketMassageService.disconnect()
+                    this.socketManageService.disconnect()
                 });
 
         }
