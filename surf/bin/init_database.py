38f515b32b4d7f53690ccac0e0d052bab8cd6882
ZedFeorius <110074416+ZedFeorius@users.noreply.github.com>
2024-08-14 18:35:27 +0800

    UPDATE |更新|ZedFeorius

--- surf/bin/init_database.py
+++ surf/bin/init_database.py
@@ -11,26 +11,12 @@ def execute_sql_files(base_dir, database):
     except subprocess.CalledProcessError as e:
         print(f"Error executing new_surf_sql.sql: {e}")
 
-    sql_dir = os.path.join(sql_dir, 'tables')
-
-    # 检查目录是否存在
-    if not os.path.exists(sql_dir):
-        print(f"Directory {sql_dir} does not exist.")
-        return
-
-    # 遍历并执行所有的.sql文件
-    for filename in os.listdir(sql_dir):
-        if filename.endswith(".sql"):
-            filepath = os.path.join(sql_dir, filename)
-            print(f"Executing {filepath}...")
-
-            # 构建并执行命令
-            command = f"sudo -u postgres psql -d {database} -f \"{filepath}\""
-            try:
-                subprocess.run(command, shell=True, check=True)
-                print(f"{filename} executed successfully.")
-            except subprocess.CalledProcessError as e:
-                print(f"Error executing {filename}: {e}")
+    command = f'sudo -u postgres psql -d {database} -f \"{os.path.join(sql_dir, "insert_test_data.sql")}\"'
+    try:
+        subprocess.run(command, shell=True, check=True)
+        print("database init executed successfully")
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing new_surf_sql.sql: {e}")
 
 
 if __name__ == "__main__":
